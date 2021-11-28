b7b574b86f1ba87ad4b18ab4caa868b2b36d0167
dyddlfsla <7942dyddlfsl@naver.com>
2021-11-29 01:34:29 +0900

    Sun Nov 29

--- src/chapter/three/Exercise3_6.java
+++ src/chapter/three/Exercise3_6.java
@@ -4,10 +4,10 @@ public class Exercise3_6 {
 
   public static void main(String[] args) {
     int fahrenheit = 100;
-    float celcius = (int)((5/9F * (fahrenheit - 32)) * 100 + 0.5F) / 100F;
+    float celsius = (int)((5/9F * (fahrenheit - 32)) * 100 + 0.5F) / 100F;
 
     System.out.println("Fahrenheit: " + fahrenheit);
-    System.out.println("Celcius: " + celcius);
+    System.out.println("Celsius: " + celsius);
   }
 
 }
