702911f9e8b9081b3cd0b192c7992e7adbe60348
dyddlfsla <dyddlfsla@e2u.kr>
2022-05-12 23:00:01 +0900

    Thu May 12 : Chapter07_16

---
+++ src/chapter/seven_2nd/_16/Chapter07_16.java
@@ -0,0 +1,15 @@
+package chapter.seven_2nd._16;
+
+public class Chapter07_16 {
+
+  /** ◆ static import 문
+   *
+   * import 문을 사용하면 클래스의 패키지명을 생략할 수 있는 것과 같이 static import 문을 사용하면 static 멤버를 호출할 때
+   * 클래스 이름을 생략할 수 있다. 특정 클래스의 static 멤버를 자주 사용할 때 편리하다. 그리고 코드도 더 간결해진다.
+   *
+   *
+   *
+   *
+   * */
+
+}
