58e7e331e2de25d9b35d28cb0c1a8e671175fdef
dyddlfsla <dyddlfsla@e2u.kr>
2022-06-10 23:17:26 +0900

    Fri Jun 10 : Chapter08_18

--- src/chapter/eight/_18/Chapter08_18.java
+++ src/chapter/eight/_18/Chapter08_18.java
@@ -19,10 +19,23 @@ public class Chapter08_18 {
    * }
    *
    * 먼저 InstallException 을 생성한 후에, initCause() 로 SpaceException 을 InstallException 의 원인 예외로 등록한다.
+   * 그리고 'throw' 로 이 예외를 던진다. initCause() 는 Exception 클래스의 조상인 Throwable 클래스에 정의되어 있기 떄문에
+   * 모든 예외에서 사용 가능하다.
    *
+   * ① Throwable initCause(Throwable cause) : 지정한 예외를 원인 예외로 등록
+   * ② Throwable getCause() : 원인 예외를 반환
+   *
+   * 발생한 예외를 그냥 처리하면 될 텐데, 왜 원인 예외로 등록해서 다시 예외를 발생시키는지 궁금할 것이다. 그 이유는 여러가지 예외를
+   * 하나의 큰 분류의 예외로 묶어서 다루기 위해서이다. 그렇다고 InstallException 을 SpaceException 과 MemoryException 의 조상으로
+   * 해서 catch 블럭을 작성하면, 실제로 발생한 예외가 어떤 것인지 알 수 없다는 문제가 생긴다. 그리고 SpaceException 과
+   * MemoryException 의 상속 관계를 변경해야 한다는 것도 부담이다.
+   *
+   * try {
+   *   startInstall(); //SpaceException 발생
+   * }
    *
    *
    *
    * */
-
 }
+
---
+++ src/chapter/test/Test.java
@@ -0,0 +1,37 @@
+package chapter.test;
+
+public class Test {
+
+  public static void main(String[] args) {
+    Father son = new Son();
+    System.out.println(son.name); //
+    System.out.println(son.age);
+    son.walk();
+    System.out.println("==============");
+    Son son1 = (Son) son;
+    System.out.println(son1.name);
+    System.out.println(son1.age);
+    son1.walk();
+  }
+
+}
+
+class Father {
+
+  String name = "jason";
+  int age = 50;
+
+  void walk() {
+    System.out.println("Father 가 걷는다.");
+  }
+}
+
+class Son extends Father {
+
+  String name = "robert";
+  int age = 10;
+
+  void walk() {
+    System.out.println("Son 이 걷는다.");
+  }
+}
