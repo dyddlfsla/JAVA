17c5024a4002ead047b5100ddec4c5765d5abd88
dyddlfsla <7942dyddlfsl@naver.com>
2022-06-18 00:00:13 +0900

    Fri Jun 17 : Chapter11_34

---
+++ src/chapter/eleven/_34/Chapter11_34.java
@@ -0,0 +1,20 @@
+package chapter.eleven._34;
+
+public class Chapter11_34 {
+
+  /** ◆ HashSet
+   *
+   * HashSet 은 Set 인터페이스를 구현한 가장 대표적인 컬렉션이며, Set 인터페이스의 특징대로 HashSet 은 중복된 요소를 저장하지 않는다.
+   * HashSet 에 새로운 요소를 추가할 떄는 add 메서드나 addAll 메서드를 사용하는데, 만일 HashSet 에 이미 저장되어 있는 요소와 중복된 요소를
+   * 추가하고자 한다면 이 메서드들은 false 를 반환함으로써 중복된 요소이기 때문에 추가에 실패했다는 것을 알린다.
+   *
+   * 이러한 HashSet 의 특징을 이용하면, 컬렉션 내의 중복된 요소들을 쉽게 제거할 수 있다.
+   *
+   * ArrayList 와 같이 List 인터페이스를 구현한 컬렉션과 달리 HashSet 은 저장 순서를 유지하지 않으므로 저장순서를 유지하고자 한다면
+   * LinkedHashSet 을 사용해야 한다.
+   *
+   *
+   *
+   * */
+
+}
