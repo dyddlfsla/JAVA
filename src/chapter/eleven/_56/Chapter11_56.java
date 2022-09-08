7bec4d5e706f46255859e777829fb4e77b7d746f
dyddlfsla <dyddlfsla@e2u.kr>
2022-09-08 23:16:17 +0900

    Thu Sep 08 : Chapter11_56

---
+++ src/chapter/eleven/_56/Chapter11_56.java
@@ -0,0 +1,25 @@
+package chapter.eleven._56;
+
+public class Chapter11_56 {
+
+
+  /*
+  * ◆ 컬렉션 클래스 정리 & 요약
+  *
+  * 지금까지 소개한 컬렉션 클래스의 특징과 관계를 다시 한번 정리해보았다. 각 컬렉션 클래스마다 장단점이 있으므로
+  * 그 구현 원리와 특징을 잘 이해한 후 상황에 가장 적합한 것을 선택해서 사용해야 할 것이다.
+  *
+  * 1. ArrayList: 배열기반, 데이터의 추가와 삭제에 불리. 배열의 맨 앞이나 맨 뒤를 수정하는 순차적인 추가삭제는 제일 빠름. 임의의 요소에 대한 접근성(accessibility) 이 뛰어남.
+  * 2. LinkedList: 각 요소들이 노드를 바탕으로 서로 참조하며 연결된 구조. 데이터 기반의 추가와 삭제에 유리. 임의의 요소에 대한 접근성은 좋지 않음.
+  * 3. HashMap: 배열과 연결이 결합된 형태. 추가, 삭제, 검색, 접근성이 모두 뛰어남. 검색에는 최고 성능을 보인다.
+  * 4. TreeMap: 연결 기반. 정렬과 검색(특히 범위검색)에 적합. 검색 성능은 HashMap 보다 떨어짐.
+  * 5. Stack: Vector 를 상속받아 구현
+  * 6. Queue: LinkedList 가 Queue 인터페이스를 구현
+  * 7. Properties: Hashtable 을 상속받아 구현
+  * 8. HashSet: HashMap 을 이용해서 구현
+  * 9. TreeSet: TreeMap 을 이용해서 구현
+  * 10. LinkedHashMap, LinkedHashSet: HashMap 과 HashSet 에 저장순서 유지 기능을 추가
+  *
+  *
+  * */
+}
