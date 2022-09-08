de43e510a81bfaed4c2fc68c4b5cc1e62ec01b80
dyddlfsla <dyddlfsla@e2u.kr>
2022-09-08 23:59:09 +0900

    Thu Sep 08 : exercise 11_1

---
+++ src/chapter/eleven/exercise/Ex11_1.java
@@ -0,0 +1,16 @@
+package chapter.eleven.exercise;
+
+public class Ex11_1 {
+
+  /*
+  * 다음 코드의 실행 결과를 적으시오.
+  *
+  * sol:
+  * list = [3, 6, 2, 2, 2, 7]
+  * set = [3, 6, 2, 7] Set 은 저장 순서를 유지하지 않고 중복을 허용하지 않음.
+  * tset = [3, 6, 2, 7]
+  *
+  *
+  * */
+
+}
