e9420df30e7b6dc47a87beb11c3f995e853d4034
dyddlfsla <7942dyddlfsl@naver.com>
2022-09-24 02:37:13 +0900

    Sat Sep 24 : solution

--- src/chapter/twelve/exercise/_03/Ex12_3.java
+++ src/chapter/twelve/exercise/_03/Ex12_3.java
@@ -1,6 +1,15 @@
 package chapter.twelve.exercise._03;
 
-class Fruit {
+class Tree {
+
+}
+
+class Fruit extends Tree {
+
+
+}
+
+class Apple extends Fruit {
 
 
 }
@@ -27,12 +36,20 @@ public class Ex12_3 {
    * Box<?> b = new Box(); → 정상. <?> 와일드카드는 모든 타입을 허용하는 것으로 제네릭에서 타입 매개변수로 Object 가 적용된 것과 같으며,
    *                         new Box(); 역시 제네릭 타입을 생략하면 기본적으로 Object 가 지정되므로 대입에 문제가 없다.
    * Box<?> b = new Box<>(); → 정상. <>(); 에서 <?> 가 생략된 것으로 문제가 없다.
-   * Box<?> b = new Box<Object>(); → 정상. <?> 는 모든 타입을 허용하므로 문제가 없다.
+   * Box<?> b = new Box<Object>(); → 에러. Box 클래스에서 타입 변수는 <T extends Fruit> 로써 이미 제한되어 있다.
+   *                                 따라서 Box<?> 는 Box<Object> 가 아닌 Box<? extends Fruit> 가 생략된 것이라고 볼 수 있다.
+   *                                 당연히 생성자의 Object 타입은 Fruit 의 하위타입이 아니므로 에러가 발생한다.
+   *
    * Box<Object> b = new Box<Fruit>(); → 에러. 기본적으로 제네릭 타입 간의 다형성은 성립하지 않으므로 에러가 발생한다.
    * Box<? extends Fruit> b = new Box<Apple>(); →  정상. 생성자의 타입 변수로 Fruit 과 그 자손들이 가능하므로 문제가 없다.
-   * Box<? extends Object> b = new Box<? extends Fruit>(); → 정상. 생성자의 타입변수로 모든 타입이 가능하므로 <? extends Fruit> 도 가능하다.
+   * Box<? extends Object> b = new Box<? extends Fruit>(); → 에러. new 연산자를 호출할 때는 지정된 타입이 명확해야 하는데 <? extends Fruit> 는 명확하지 않다.
    *
    *
    *
    * */
+
+  public static void main(String[] args) {
+    Box<? extends Fruit> box = new Box<Apple>();
+    System.out.println(box.getClass());
+  }
 }
