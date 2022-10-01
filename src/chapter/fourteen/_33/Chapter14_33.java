b1b2b605340d399306a509044f8dbab855b10334
dyddlfsla <7942dyddlfsl@naver.com>
2022-10-02 03:10:53 +0900

    Sun Oct 02 : AppMain

--- src/chapter/fourteen/_33/Chapter14_33.java
+++ src/chapter/fourteen/_33/Chapter14_33.java
@@ -19,7 +19,13 @@ public class Chapter14_33 {
   *  Stream<Stream<String>> strStrStream = strStream.map(Arrays::stream));
   *
   * 예상한 것과 달리, Stream<String[]> 을 'map(Arrays::stream)' 으로 변환한 결과는 Stream<String> 이 아니라 Stream<Stream<String>> 이다.
-  * 즉, 스트림 속에 스트림이 또 들어간 형태인 것이다.
+  * 즉, 스트림 속에 스트림이 또 들어간 형태인 것이다. 각 요소의 문자열이 합쳐지지 않고 스트림의 스트림 형태가 되어버렸다.
+  *
+  * 이때 사용할 수 있는 것이 flatMap() 이다.
+  * flatMap() 이란 쉽게 말해, 요소를 대체하는 복수 개의 요소들로 새로운 스트림을 구성하여 리턴한다.
+  * 스트림에서 A 라는 요소가 a1, a2 요소로 대체되고, B 라는 요소는 b1, b2 로 대체된다고 가정했을 경우,
+  * a1, a2, b1, b2 를 요소로 가지는 새로운 스트림이 반환되는 것이다.
+  *
   *
   *
   *
